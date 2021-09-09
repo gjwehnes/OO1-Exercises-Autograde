2f46fb4d24b69bbc9d52f5f7cbbefe177b73f1e3
gjwehnes <gjwehnes@L829WD150008.edu.cbe.ab.ca>
2021-09-09 14:26:16 -0600

    cleaned up instructions for ex010

--- src/ex010/StoreProduct.java
+++ src/ex010/StoreProduct.java
@@ -3,19 +3,19 @@ package ex010;
 /*
 1. Complete the StoreProduct.java class:
 
-Include the following class variables:
-label
-price (type integer)
-category
-hasExpiration (type boolean)
-stock (type int, represents how many the store has available)
-All class variables should be declared case sensitive
+Include the following instance variables:
+-label (type String)
+-price (type integer)
+-category (type String)
+-hasExpiration (type boolean)
+-stock (type int, represents how many the store has available)
+All instance variables should be declared with proper code conventions
 
 Write four constructors:
-The main constructor
-A secondary constructor that does not have a category or an expiration (category defaults to "misc", hasExpiration defaults to false)
-A secondary constructor that does not have a category, expiration, or stock (stock defaults to 0)
-A secondary constructor that just does not have stock
+The main constructor, which receives values for all instance variables
+A secondary constructor that does not receive a category or an expiration (category defaults to "misc", hasExpiration defaults to false)
+A secondary constructor that does not receive a category, expiration, or stock (stock defaults to 0)
+A secondary constructor that does not receive stock
 
 2. Include the following methods:
 
@@ -26,7 +26,8 @@ public boolean makeSale(int quantity)
 Check if the quantity (being bought) is available given the stock.  If there is enough, then make the sale by subtracting the quantity from the stock and return true (the sale was successful).  If there isn't enough, return false instead.
 
 public double getDiscountedPrice(double discount)
-The parameter discount should be between .01 and 1 and represent a 0 to 100% discount.  Multiply the product's price by the (1 - discount) and return that number.
+The parameter discount should be between .01 and 1 and represent a 0 to 100% discount.  Multiply the product's price by the (1 - discount) to obtain the discounted price
+and return that value.
 
 */
 public class StoreProduct {
