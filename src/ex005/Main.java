4508e6489b5b331a2af9a4bc575538b2a6b2931a
gjwehnes <gjwehnes@shaw.ca>
2021-09-13 13:25:38 -0600

    Update Main.java
    added clarification and note to ex005 instructions

--- src/ex005/Main.java
+++ src/ex005/Main.java
@@ -1,11 +1,14 @@
 package ex005;
 
-// Within the main class, create three class variables:
+// Within the main class, create instance variables:
 //     A boolean variable called active with a value of true
 //     A String variable called name with a value of "Nemo"
 //     An integer variable called grade with a value of 11
 //
 // Access should be public
+//
+// Note: you are not creating accessors for this exercise; rather you exposing your instance variables, so that other objects
+// can directly read and/or modify them. This is perfectly legal, and in some cases perfectly sensible.
 
 public class Main {
 }
