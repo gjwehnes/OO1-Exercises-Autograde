99b1babffd3684b2a040bea78c25381b29a5fcae
gjwehnes <gjwehnes@shaw.ca>
2021-09-13 15:40:56 -0600

    Update StockTest.java
    changed for compatibility with JUnit5

--- src/ex014/StockTest.java
+++ src/ex014/StockTest.java
@@ -5,25 +5,25 @@ import org.junit.jupiter.api.Test;
 
 public class StockTest {
 
-@Test
-public void tests() {
-// Failure message: 
-// This test has no failure messages
+	@Test
+	public void tests() {
+		// Failure message: 
+		// This test has no failure messages
 		Stock s = new Stock("abc","Alphabet",10,100);
 		assertEquals("ABC",s.tickerSymbol);
 		assertEquals("Alphabet", s.companyName);
-		assertEquals(10, s.price, 0);
+		assertEquals(10, s.price, 0.001);
 		assertEquals(100, s.totalShares);
 
-		assertEquals(0,s.percentChange,0.0001);
-		assertEquals(1000,s.marketCap, 0);
+		assertEquals(0,s.percentChange,0.001);
+		assertEquals(1000,s.marketCap, 0.001);
 
 		s.adjustPrice(-5);
 		assertEquals(-50,s.percentChange,0.001);
-		assertEquals(500,s.marketCap, 0);
+		assertEquals(500,s.marketCap, 0.001);
 
 		s.adjustPrice(3);
 		assertEquals(60,s.percentChange,0.001);
-		assertEquals(800,s.marketCap, 0);
-}
+		assertEquals(800,s.marketCap, 0.001);
+	}
 }
