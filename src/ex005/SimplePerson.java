4e80bff9b3ec5bda5e82f94afea9bfa81c929274
gjwehnes <gjwehnes>
2022-02-13 12:39:42 -0700

    revised ex005

--- src/ex005/SimplePerson.java
+++ src/ex005/SimplePerson.java
@@ -1,14 +1,13 @@
 package ex005;
 
 // Within the main class, create instance variables:
-//     A boolean variable called active with a value of true
-//     A String variable called name with a value of "Nemo"
-//     An integer variable called grade with a value of 11
+//     A String variable called firsName with a value of "Billy"
+//     An integer variable called age with a value of 16
 //
 // Access should be public
 //
 // Note: you are not creating accessors for this exercise; rather you exposing your instance variables, so that other objects
 // can directly read and/or modify them. This is perfectly legal, and in some cases perfectly sensible.
 
-public class Main {
+public class SimplePerson {
 }
